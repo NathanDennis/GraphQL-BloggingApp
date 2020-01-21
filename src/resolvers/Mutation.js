b8a2ddd6cccd61aadf61fdea528c1e3f671af116
Nathan <nathdennis@googlemail.com>
2020-01-21 20:56:00 +0000

    Added mutations to update User, Post & Comment

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -44,6 +44,36 @@ const Mutation = {
 
         return deletedUsers[0]
     },
+    // UPDATE USER
+    updateUser(parent, args, {db}, info){
+        const {id, data} = args
+        const user = db.users.find((user) => user.id === id)
+        // Check for user match
+        if (!user) {
+            throw new Error('User not found')
+        }
+
+        // Update details if provided
+        if (typeof data.email === 'string'){
+            const emailTaken = db.users.some((user) => user.email === data.email)
+
+            if(emailTaken){
+                throw new Error('Email already in use')
+            }
+
+            user.email = data.email
+        }
+
+        if (typeof data.name === 'string'){
+            user.name = data.name
+        }
+
+        if (typeof data.age !== 'undefined') {
+            user.age = data.age
+        }
+
+        return user
+    },
     // CREATE POST
     createPost(parent, args, {db}, info) {
         const userExists = db.users.some((user) => user.id === args.data.author)
@@ -78,6 +108,29 @@ const Mutation = {
         return deletedPosts[0]
 
     },
+    // UPDATE POST
+    updatePost(parent, args, {db}, info){
+        const {id, data} = args
+        const post = db.posts.find((post) => post.id === id)
+
+        if(!post){
+            throw new Error('Post not found')
+        }
+
+        if(typeof data.title === 'string'){
+            post.title = data.title
+        }
+
+        if(typeof data.body === 'string'){
+            post.body = data.body
+        }
+
+        if(typeof data.published === 'boolean'){
+            post.published = data.published
+        }
+
+        return post
+    },
     // CREATE COMMENT
     createComment(parent, args, {db}, info) {
         // if post && user exist - Create the comment
@@ -108,6 +161,20 @@ const Mutation = {
         const deletedComments = db.comments.splice(commentIndex, 1)
 
         return deletedComments[0]
+    },
+    updateComment(parent, args, {db}, info){
+        const {id, data} = args
+        const comment = db.comments.find((comment) => comment.id === id)
+
+        if(!comment){
+            throw new Error('Comment not found')
+        }
+
+        if(typeof data.text === 'string'){
+            comment.text = data.text
+        }
+
+        return comment
     }
 }
 
